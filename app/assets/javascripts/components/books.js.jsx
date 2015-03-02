3598cbf8c4be45128dcb04c97e6e0b1f68b76642
Mateusz Palyz <m.palyz@pilot.co>
2015-03-02 22:56:30 +0100

    add simple books list example

--- app/assets/javascripts/components/books.js.jsx
+++ app/assets/javascripts/components/books.js.jsx
@@ -1,14 +1,63 @@
-var App = React.createClass({
+/** @jsx React.DOM */
+
+var BooksBox = React.createClass({
+  loadBooksFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadBooksFromServer();
+  },
   render: function() {
     return (
-      <h1>ble ble</h1>
+      <div>
+        <h1>Books</h1>
+        <BooksList data={this.state.data} />
+      </div>
     );
   }
 });
 
-$(function(){
+var BooksList = React.createClass({
+  render: function() {
+    var booksNodes = this.props.data.map(function(book, index) {
+      return (
+        <Book book={book} key={index} />
+      );
+    });
+    return (
+      <div>
+        {booksNodes}
+      </div>
+    );
+  }
+});
+
+var Book = React.createClass({
+  render: function() {
+    return (
+      <ul>
+        <li>{this.props.book.title}</li>
+        <li>{this.props.book.author}</li>
+      </ul>
+    );
+  }
+});
+
+$(document).on("page:change", function() {
   React.render(
-    <App />,
+    <BooksBox url="api/books.json" />,
     document.getElementById('content')
   );
 })
